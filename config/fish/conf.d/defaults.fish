62dc7f69bc69ca50f3f44fdb41937ab43afd7a09
Jackson Law <178053+jlaw@users.noreply.github.com>
2018-12-12 23:32:51 -0800

    Manually install binaries

--- config/fish/conf.d/defaults.fish
+++ config/fish/conf.d/defaults.fish
@@ -34,15 +34,11 @@ set -Ux CCACHE_DIR $XDG_CACHE_HOME/ccache
 set -Ux FZF_DEFAULT_COMMAND 'fd --type file --type symlink'
 
 # plugin (commands)
-set -U FZF_OPEN_COMMAND $FZF_DEFAULT_COMMAND
 set -U FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 set -U FZF_CD_COMMAND 'fd --type directory --follow'
 set -U FZF_CD_WITH_HIDDEN_COMMAND 'fd --type directory --follow --hidden --exclude .git'
+set -U FZF_OPEN_COMMAND $FZF_DEFAULT_COMMAND
 
-# plugin (preview)
-set -U FZF_ENABLE_OPEN_PREVIEW 1
-set -U FZF_PREVIEW_FILE_COMMAND 'head -n 10'
-set -U FZF_PREVIEW_DIR_COMMAND 'ls'
-
-# local
-set -U FZF_BASE_OPTS "--no-bold"
+# plugin (ui)
+set -U FZF_COMPLETE 2
+set -U FZF_LEGACY_KEYBINDINGS 0
